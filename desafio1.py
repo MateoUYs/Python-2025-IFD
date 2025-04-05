ddb5effa34f29b19b7d45b5562d2e889fd2cf9a0
Mateo <mateoindart100@gmail.com>
2025-04-04 23:33:30 -0300

    DESAFIO 1 Y 2

---
+++ desafio1.py
@@ -0,0 +1,13 @@
+ancho = 4
+largo = 5
+
+suma = ancho + largo
+print(f"La suma de {ancho} y {largo} es {suma}")
+
+#---------------------------
+#DESAFIO 2: AREA DE UN RECTANGULO
+
+area_rectangulo = ancho * largo
+mensaje = f"Se calculará el área de un rectángulo de ancho {ancho} y largo {largo}, cuya área será {area_rectangulo}"
+
+print(mensaje)
